62b8a79a501ceccc14160021047696c00856a420
Yip Jun Han <junhan.y_dcube@scientecpeople.com>
2023-04-12 16:29:56 +0800

    [CCUBE-167][JH] add JsonPathUtils

---
+++ JsonPathUtils.ts
@@ -0,0 +1,87 @@
+import { JSONPath } from "jsonpath-plus";
+import * as _ from "lodash";
+
+export namespace JsonPathUtils {
+	type TData = string | number | boolean | object | any[] | null;
+	interface TStringPattern {
+		stringPattern: string;
+	}
+	interface TObjectPattern {
+		objectPattern: string;
+		unwrap?: boolean;
+		parseString?: string;
+	}
+
+	type TPattern = string | number | boolean | TStringPattern | TObjectPattern;
+	type TReturn<T extends TPattern> = T extends TObjectPattern
+		? T extends { parseString: "boolean" }
+			? boolean
+			: T extends { parseString: "number" }
+			? number
+			: T extends { parseString: "array" }
+			? any[]
+			: T extends { parseString: "datetime" }
+			? string
+			: object | any[]
+		: T extends TStringPattern
+		? string
+		: T;
+
+	const isStringPattern = (x: any): x is TStringPattern => (x as TStringPattern).stringPattern !== undefined;
+	const isObjectPattern = (x: any): x is TObjectPattern => (x as TObjectPattern).objectPattern !== undefined;
+	const falseyList = ["false", "0", "-0", "0n", "", "null", "undefined", "nan"];
+
+	const getResultFromStringPattern = (pattern: TStringPattern, data: TData): TReturn<TStringPattern> => {
+		const parsedPatternsList = pattern.stringPattern.matchAll(/\{\{(.*?)\}\}/g);
+		let output = pattern.stringPattern;
+
+		for (const parsedPattern of parsedPatternsList) {
+			let result = JSONPath({ path: parsedPattern[1], json: data, wrap: false });
+			if (Array.isArray(result)) {
+				if (result.length === 1) result = result[0];
+				else result = JSON.stringify(result);
+			}
+			output = output.replace(parsedPattern[0], result === undefined ? "" : result);
+		}
+
+		return output;
+	};
+
+	const getResultFromObjectPattern = (pattern: TObjectPattern, data: TData): TReturn<TObjectPattern> => {
+		let result = JSONPath({ path: pattern.objectPattern, json: data, wrap: !pattern.unwrap });
+		if (pattern.unwrap) {
+			if (Array.isArray(result) && result.length === 1) result = result[0];
+			if (pattern.parseString) {
+				switch (pattern.parseString) {
+					case "number":
+						result = Number(result);
+						break;
+					case "boolean":
+						result =
+							typeof result === "string" ? !falseyList.includes(result.toLowerCase()) : Boolean(result);
+						break;
+					case "array":
+						try {
+							const parsedArr = JSON.parse(result.replace(/'/g, '"'));
+							result = _.isArray(parsedArr) ? parsedArr : null;
+						} catch (e) {
+							result = null;
+						}
+						break;
+					default:
+						throw new Error(`Invalid "parseString" value "${pattern.parseString}"`);
+				}
+			}
+		}
+		return result;
+	};
+
+	export const parse = <T extends TPattern>(pattern: T, data: TData): TReturn<T | TPattern> => {
+		if (isStringPattern(pattern)) {
+			return getResultFromStringPattern(pattern, data);
+		} else if (isObjectPattern(pattern)) {
+			return getResultFromObjectPattern(pattern, data);
+		}
+		return pattern;
+	};
+}
---
+++ __tests__/JsonPathUtils.spec.ts
@@ -0,0 +1,293 @@
+import { JsonPathUtils } from "../JsonPathUtils";
+
+/* tslint:disable-next-line no-big-function */
+describe("JsonPathUtils", () => {
+	const data = {
+		firstName: "John",
+		lastName: "doe",
+		age: 26,
+		address: {
+			streetAddress: "naist street",
+			city: "Nara",
+			postalCode: "630-0192",
+		},
+		countString: "123",
+		boolString: "true",
+		stringifiedArray: "['a', 'b']",
+		emptyArray: [],
+		nullAttr: null,
+		boolAttr: false,
+		floatAttr: 12.3,
+		phoneNumbers: [
+			{
+				type: "iPhone",
+				number: "0123-4567-8888",
+			},
+			{
+				type: "home",
+				number: "0123-4567-8910",
+			},
+			{
+				type: "iPhone",
+				number: "0123-4567-1234",
+			},
+		],
+	};
+
+	describe("StringPattern", () => {
+		it("should return correct output for string input", () => {
+			const pattern = "just a string";
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("just a string");
+		});
+
+		it("should return correct output for number input", () => {
+			const pattern = 123;
+			expect(JsonPathUtils.parse(pattern, data)).toEqual(123);
+		});
+
+		it("should return correct output for single string pattern in array", () => {
+			const pattern = { stringPattern: "{{$.phoneNumbers[0].number}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("0123-4567-8888");
+		});
+
+		it("should return correct output for multiple patterns in string", () => {
+			const pattern = { stringPattern: "{{$.lastName}}, {{$.lastName}}! {{$.firstName}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("doe, doe! John");
+		});
+
+		it("should return correct output for string filter pattern", () => {
+			const pattern = { stringPattern: "{{$.phoneNumbers[?(@.type=='home')].number}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("0123-4567-8910");
+		});
+
+		it("should return correct output for string filter pattern with multiple results", () => {
+			const pattern = { stringPattern: "{{$.phoneNumbers[?(@.type=='iPhone')].number}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual(`[\"0123-4567-8888\",\"0123-4567-1234\"]`);
+		});
+
+		it("should return empty string for string pattern with no results", () => {
+			const pattern = { stringPattern: "{{$.abc}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("");
+		});
+
+		it("should return empty string/correct output for string pattern with some results", () => {
+			const pattern = { stringPattern: "{{$.abc}}, {{$.lastName}}! {{$.firstName}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual(", doe! John");
+		});
+
+		it("should return empty string/correct output for string filter pattern with some results", () => {
+			const pattern = {
+				stringPattern:
+					"{{$.phoneNumbers[?(@.type=='iPhone')].number}}, {{$.phoneNumbers[?(@.type=='abc')].number}}",
+			};
+			expect(JsonPathUtils.parse(pattern, data)).toEqual(`[\"0123-4567-8888\",\"0123-4567-1234\"], `);
+		});
+
+		it("should return correct output for string pattern with actual null result", () => {
+			const pattern = { stringPattern: "{{$.nullAttr}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("null");
+		});
+
+		it("should return correct output for string pattern with actual empty array result", () => {
+			const pattern = { stringPattern: "{{$.emptyArray}}" };
+			expect(JsonPathUtils.parse(pattern, data)).toEqual("[]");
+		});
+	});
+
+	describe("ObjectPattern", () => {
+		describe("Unwrap: false", () => {
+			it("should return correct output for object pattern", () => {
+				const pattern = { objectPattern: "$.age" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([26]);
+			});
+
+			it("should return correct output for object pattern, string result", () => {
+				const pattern = { objectPattern: "$.countString" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(["123"]);
+			});
+
+			it("should return correct output for object pattern, obj result", () => {
+				const pattern = { objectPattern: "$.address" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([
+					{
+						streetAddress: "naist street",
+						city: "Nara",
+						postalCode: "630-0192",
+					},
+				]);
+			});
+
+			it("should return correct output for object filter pattern", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='home')]" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([{ type: "home", number: "0123-4567-8910" }]);
+			});
+
+			it("should return correct output for object filter pattern, multiple results", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='iPhone')]" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([
+					{ type: "iPhone", number: "0123-4567-8888" },
+					{ type: "iPhone", number: "0123-4567-1234" },
+				]);
+			});
+
+			it("should return correct output for object pattern, no results", () => {
+				const pattern = { objectPattern: "$.abc" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([]);
+			});
+
+			it("should return correct output for object filter pattern, no results", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='abc')]" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([]);
+			});
+
+			it("should return correct output for object pattern with actual null result", () => {
+				const pattern = { objectPattern: "$.nullAttr" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([null]);
+			});
+
+			it("should return correct output for object pattern with actual empty array result", () => {
+				const pattern = { objectPattern: "$.emptyArray" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([[]]);
+			});
+		});
+
+		describe("Unwrap: true", () => {
+			it("should return correct output for object pattern with unwrap:true", () => {
+				const pattern = { objectPattern: "$.age", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(26);
+			});
+
+			it("should return correct output for object pattern with unwrap:true, string result", () => {
+				const pattern = { objectPattern: "$.countString", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual("123");
+			});
+
+			it("should return correct output for object pattern with unwrap:true, obj result", () => {
+				const pattern = { objectPattern: "$.address", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual({
+					streetAddress: "naist street",
+					city: "Nara",
+					postalCode: "630-0192",
+				});
+			});
+
+			it("should return correct output for object filter pattern with unwrap:true", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='home')]", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual({ type: "home", number: "0123-4567-8910" });
+			});
+
+			it("should return correct output for object filter pattern with unwrap:true, multiple results", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='iPhone')]", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([
+					{ type: "iPhone", number: "0123-4567-8888" },
+					{ type: "iPhone", number: "0123-4567-1234" },
+				]);
+			});
+
+			it("should return correct output for object pattern with unwrap:true, no results", () => {
+				const pattern = { objectPattern: "$.abc", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(undefined);
+			});
+
+			it("should return correct output for object filter pattern with unwrap:true, no results", () => {
+				const pattern = { objectPattern: "$.phoneNumbers[?(@.type=='abc')]", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(undefined);
+			});
+
+			it("should return correct output for object pattern with unwrap:true, with actual null result", () => {
+				const pattern = { objectPattern: "$.nullAttr", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(null);
+			});
+
+			it("should return correct output for object pattern with unwrap:true, with actual empty array result", () => {
+				const pattern = { objectPattern: "$.emptyArray", unwrap: true };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual([]);
+			});
+		});
+	});
+
+	describe("ObjectPattern (parseString)", () => {
+		describe("Number", () => {
+			it("should return correct output for parseString number", () => {
+				const pattern = { objectPattern: "$.countString", unwrap: true, parseString: "number" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(123);
+			});
+
+			it("should return correct output for parseString number, floatStr result", () => {
+				const pattern = { objectPattern: "$.floatStr", unwrap: true, parseString: "number" };
+				expect(JsonPathUtils.parse(pattern, { ...data, floatStr: "12.3" })).toEqual(12.3);
+			});
+
+			it("should return correct output for parseString number, letter string result", () => {
+				const pattern = { objectPattern: "$.firstName", unwrap: true, parseString: "number" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(NaN);
+			});
+
+			it("should return correct output for parseString number, null result", () => {
+				const pattern = { objectPattern: "$.nullAttrStr", unwrap: true, parseString: "number" };
+				expect(JsonPathUtils.parse(pattern, { ...data, nullAttrStr: "null" })).toEqual(NaN);
+			});
+
+			it("should return correct output for parseString number, no results", () => {
+				const pattern = { objectPattern: "$.abc", unwrap: true, parseString: "number" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(NaN);
+			});
+		});
+
+		describe("Boolean", () => {
+			it("should return correct output for parseString boolean", () => {
+				const pattern = { objectPattern: "$.boolString", unwrap: true, parseString: "boolean" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(true);
+			});
+
+			it("should return correct output for parseString boolean, letter string result", () => {
+				const pattern = { objectPattern: "$.firstName", unwrap: true, parseString: "boolean" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(true);
+			});
+
+			it("should return correct output for parseString boolean, boolean with non-lowercase result", () => {
+				const pattern = { objectPattern: "$.boolStringCaps", unwrap: true, parseString: "boolean" };
+				expect(JsonPathUtils.parse(pattern, { ...data, boolStringCaps: "False" })).toEqual(false);
+			});
+
+			it("should return correct output for parseString boolean, null result", () => {
+				const pattern = { objectPattern: "$.nullAttr", unwrap: true, parseString: "boolean" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(false);
+			});
+
+			it("should return correct output for parseString boolean, no results", () => {
+				const pattern = { objectPattern: "$.abc", unwrap: true, parseString: "boolean" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(false);
+			});
+		});
+
+		describe("Array", () => {
+			it("should return correct output for parseString array", () => {
+				const pattern = { objectPattern: "$.stringifiedArray", unwrap: true, parseString: "array" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(["a", "b"]);
+			});
+
+			it("should return correct output for parseString array, non-parseable result", () => {
+				const pattern = { objectPattern: "$.firstName", unwrap: true, parseString: "array" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(null);
+			});
+
+			it("should return correct output for parseString array, parseable non-array result", () => {
+				const pattern = { objectPattern: "$.age", unwrap: true, parseString: "array" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(null);
+			});
+
+			it("should return correct output for parseString array, no results", () => {
+				const pattern = { objectPattern: "$.abc", unwrap: true, parseString: "array" };
+				expect(JsonPathUtils.parse(pattern, data)).toEqual(null);
+			});
+		});
+
+		it("should return error for invalid parseString", () => {
+			const pattern = { objectPattern: "$.stringifiedArray", unwrap: true, parseString: "abc" };
+			expect(() => JsonPathUtils.parse(pattern, data)).toThrowError(
+				new Error(`Invalid "parseString" value "${pattern.parseString}"`),
+			);
+		});
+	});
+});
