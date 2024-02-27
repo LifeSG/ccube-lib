417df591baa295e93a87875e4bd79e0b6fa345c8
Quek Ruo Ling <ruoling.quek@ufinity.com>
2024-02-27 13:35:51 +0800

    [CCUBE-1004][RL] Add new edit status for session

--- src/sequential/common/session.ts
+++ src/sequential/common/session.ts
@@ -11,6 +11,7 @@ import type { Nullable } from "../../types";
 export enum SessionStatus {
     SUCCESS = "success",
     INCOMPLETE = "incomplete",
+    EDIT = "edit",
 }
 
 export enum UserLoginType {
